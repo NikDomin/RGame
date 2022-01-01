a3aa484c35ec7dacf32a44fd5f8b96cf8395b4a9
nik jow <nikolaydiomin18@gmail.com>
2022-01-01 12:37:32 +0200

    Добавьте файлы проекта.

---
+++ RG/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RLNET;
+
+namespace RG
+{
+    public class Game
+    {
+        // The screen height and width are in number of tiles
+        private static readonly int _screenWidth = 100;
+        private static readonly int _screenHeight = 70;
+        private static RLRootConsole _rootConsole;
+        // Map Console 
+        private static readonly int _mapWidth = 80;
+        private static readonly int _mapHeight = 48;
+        private static RLConsole _mapConsole;
+        // Message Console 
+        private static readonly int _messageWidth = 80;
+        private static readonly int _messageHeight = 11;
+        private static RLConsole _messageConsole;
+        // Stat Console
+        private static readonly int _statWidth = 20;
+        private static readonly int _statHeight = 70;
+        private static RLConsole _statConsole;
+        // Inventory Console
+        private static readonly int _inventoryWidth = 80;
+        private static readonly int _inventoryHeight = 11;
+        private static RLConsole _inventoryConsole;
+
+        // Event handler for RLNET's Update event
+        private static void OnRootConsoleUpdate(object sender, UpdateEventArgs e)
+        {
+            //_rootConsole.Print(50, 50, "It worked!", RLColor.White);
+            _mapConsole.SetBackColor(0, 0, _mapWidth, _mapHeight, RLColor.Black);
+            _mapConsole.Print(1, 1, "Map", RLColor.White);
+
+            _messageConsole.SetBackColor(0, 0, _messageWidth, _messageHeight, RLColor.Gray);
+            _messageConsole.Print(1, 1, "Messages", RLColor.White);
+
+            _statConsole.SetBackColor(0, 0, _statWidth, _statHeight, RLColor.Brown);
+            _statConsole.Print(1, 1, "Stats", RLColor.White);
+
+            _inventoryConsole.SetBackColor(0, 0, _inventoryWidth, _inventoryHeight, RLColor.Cyan);
+            _inventoryConsole.Print(1, 1, "Inventory", RLColor.White);
+        }
+        private static void OnRootConsoleRender(object sender, UpdateEventArgs e)
+        {
+            // Blit the sub consoles to the root console in the correct locations
+            RLConsole.Blit(_mapConsole, 0, 0, _mapWidth, _mapHeight,
+              _rootConsole, 0, _inventoryHeight);
+            RLConsole.Blit(_statConsole, 0, 0, _statWidth, _statHeight,
+              _rootConsole, _mapWidth, 0);
+            RLConsole.Blit(_messageConsole, 0, 0, _messageWidth, _messageHeight,
+              _rootConsole, 0, _screenHeight - _messageHeight);
+            RLConsole.Blit(_inventoryConsole, 0, 0, _inventoryWidth, _inventoryHeight,
+              _rootConsole, 1, 1);
+            // Tell RLNET to draw the console that we set
+            _rootConsole.Draw();
+        }
+
+        static void Main(string[] args)
+        {
+            string fontFileName = "terminal8x8.png";
+            // Верхняя часть окна
+            string consoleTitle = "RG v.0,000001";
+            _rootConsole = new RLRootConsole(fontFileName, _screenWidth, _screenHeight, 8, 8, 1f, consoleTitle);
+            _mapConsole = new RLConsole(_mapWidth, _mapHeight);
+            _messageConsole = new RLConsole(_messageWidth, _messageHeight);
+            _statConsole = new RLConsole(_statWidth, _statHeight);
+            _inventoryConsole = new RLConsole(_inventoryWidth, _inventoryHeight);
+            _rootConsole.Update += OnRootConsoleUpdate;
+            _rootConsole.Render += OnRootConsoleRender;
+            _rootConsole.Run();
+        }
+    }
+}
